c3f61891c2cb101194fdd0b6b51edb50b35600e9
kartikballal <95964691+kartikballal@users.noreply.github.com>
2024-09-18 12:37:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+ 
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) =>{
+    res.send('kartikballal')  
+})
+
+
+app.get('login',(req, res)=> {
+     res.send('<h1>please login at chai aur code</h1>')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
